54c892af9f38c1b9e46b9c869336efd9bf6f1bb5
yukimeat1999 <yukimeat1999@gmail.com>
2022-12-05 23:14:28 +0900

    Edit Serial.cpp and Serial.h

--- Roomba_OutPort/include/Roomba_OutPort/Serial.h
+++ Roomba_OutPort/include/Roomba_OutPort/Serial.h
@@ -1,27 +1,12 @@
-﻿#pragma once
-/*
-//Function definitions
-int RobotInitialize(int* port_num);
-void action(int* port_num);
-void wait(int* port_num);
-void PWMcontrol(int* port_num, int r, int l);
-void Frash(int* port_num);
-void Torque_OFF(int* port_num);
-*/
+#pragma once
 
-//controller.h
-#define MAX_OUTPUT 150.0	// Maximum output value of omni-directional mobile robot
+#define MAX_OUTPUT 150.0	// Maximum output value
 
 
 //Function definitions
 int iRobotInitialize(const char* port_num);
-//void drive(int wheel1, int wheel2);
-//void stop(void);
 void start(void);
 void safe(void);
-//void turn(int wheel1, int wheel2);
-//void turnLeft90(void);
-//void turnRight90(void);
 void controlMotor(short r, short l);
 
 
